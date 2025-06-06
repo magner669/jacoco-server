aed55ac8229a3f2f140bd88c4c8280f3ea75ea53
magner669 <ariel.magnum@gmail.com>
2025-06-06 16:02:13 +0100

    Gradle build, spring boot integration

---
+++ src/main/java/org/magnasoft/jacoco/server/JacocoServerApplication.java
@@ -0,0 +1,13 @@
+package org.magnasoft.jacoco.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacocoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacocoServerApplication.class, args);
+	}
+
+}
